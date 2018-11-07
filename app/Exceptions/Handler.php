43776d82880eb58feb41a960ea90458ff0533378
Abeer Azad <abeerazad19@gmail.com>
2018-11-07 17:51:38 +0600

    Change in handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,5 +47,24 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+
+    }
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+
+        $guard = array_get($exception->guards(),0);
+
+        switch ($guard) {
+            case 'admin':
+                return redirect()->guest(route('admin.login'));
+                break;
+
+            default:
+                return redirect()->guest(route('login'));
+                break;
+        }
     }
 }
